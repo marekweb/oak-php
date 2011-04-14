7e49733eae609e73b31977f5eeb9a6d6cce7c189
Marek <marekz@gmail.com>
2011-04-14 12:21:32 -0400

    fix for blank app

--- apps/blankapp/init.php
+++ apps/blankapp/init.php
@@ -4,20 +4,16 @@ require '../../../init.php';
 
 $router = new \oak\Router(array(
 	'GET' => array(
-		'greetings/:name' => array('exampleapp\Controller', 'greetings'),
-		'greetings' => array('exampleapp\Controller', 'greetingsblank'),
-		'' => array('exampleapp\Controller', 'index'),
+
 	),
 
 	'POST' => array(
-		/* POST paths are separate from GET, but work the exact same way:
-			'submitpath' => array('exampleapp\Controller', 'submithandler'),
-		*/
+
 	)
 ));
 
 $invoker = new \oak\Invoker;
 
-$dispatcher = new \oak\Dispatcher($router, $invoker, array('exampleweb\Controller', 'pageNotFound'));
+$dispatcher = new \oak\Dispatcher($router, $invoker);
 
 $dispatcher->dispatchFromEnvironment();

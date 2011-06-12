09a2e536162edcd35262ea90893d0dc303cb5c8e
Marek <marekz@gmail.com>
2011-06-12 13:46:07 -0400

    Minor changes to init file

--- init.php
+++ init.php
@@ -2,11 +2,8 @@
 /**
  * Initializes the application environment.
  * This file enables library autoloading, sets up error handling, and defines the PATH constants.
- * The PATH constants are particularly useful for using the other features of Oak.
  * 
- * The purpose of this file is to have a ready configuration.
- * Nothing in this configuration is sacred. Rip it out and tear it up if you feel like it.
- * Don't be afraid to use only what you need, and modify the rest.
+ * Modify as needed.
  */
  
  
@@ -30,29 +27,15 @@ set_error_handler(function ($errno, $errstr, $errfile, $errline ) {
  * Convention: Paths have a trailing slash. Use only forward slashes, even on windows.
  * 
  * OAK_PATH: The central Oak directory.
- * LIB_PATH: The central Oak library directory.
- * OAK_LIB_PATH: Identical to LIB_PATH
- * APP_PATH: The base path of the current application. It can store its files here.
- * APP_LIB_PATH: The library directory of the application. Used as an app-local library,
- *		it can be used as a second lookup location by ClassLoader.
+ * APP_PATH: The base path of the current application.
+ * APP_PUB_PATH: The public directory of the current application. This is where the HTTP server should point.
+ * APP_RES_PATH: The resources directory in the base path of the current application.
  */
  
-/*
-	Path building reference:
-		Path of presently included file:
-			dirname(__FILE__) (varies depending on file where it's placed)
-		Path of invoked file:
-			'.' (the cwd is set to the location of the requested file, AFTER mod_rewrite)
-
-	__FILE__ already returns an absolute path while '.' is relative. We can use realpath()
-	to obtain an absolute path. This isn't really strictly necessary but in case you need
-	to uniquely identify particular files or directories, it's a good idea.
-*/
 define('OAK_PATH', dirname(__FILE__) . '/');
-define('LIB_PATH', OAK_PATH . 'lib/');
-define('OAK_LIB_PATH', 'LIB_PATH'); // Alias
-define('APP_PATH', realpath('../') . '/');
-define('APP_LIB_PATH', realpath(APP_PATH . '/lib') . '/');
+define('APP_PUB_PATH', realpath('.') . '/');
+define('APP_PATH', realpath(APP_PUB_PATH . '/..') . '/');
+define('APP_RES_PATH', realpath(APP_PATH . '/resources') . '/');
 
 /**
  * Library autoloading
@@ -64,12 +47,10 @@ define('APP_LIB_PATH', realpath(APP_PATH . '/lib') . '/');
  * In other words case sensitivity of the file system will make a difference here.
  * The convention in the Oak library has been to use lowercase namespaces and camelcase classes.
  */
-include LIB_PATH . 'oak/ClassLoader.php';
+include OAK_PATH . 'oak/ClassLoader.php';
 
-$classLoader = new \oak\ClassLoader(array(LIB_PATH, APP_LIB_PATH));
+$classLoader = new \oak\ClassLoader(array(OAK_PATH));
 
-// Alternative search order: let the app lib override the central lib
-//$classLoader = new \oak\ClassLoader(array(APP_LIB_PATH, LIB_PATH));
 
 $classLoader->register(); 
 // It's not a brilliant idea to use methods that have side-effects.
